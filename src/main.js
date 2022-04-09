a88796608943216d54480606f41db2fe108ce574
Dominika Koźmin <63313300+Eiffla@users.noreply.github.com>
2022-04-09 14:40:43 +0200

    Added a new functions
    Setup connection with DOM elements and created event listener.

---
+++ src/main.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', initializeApp);
+
+const viewElements = {};
+
+const getElement = id => {
+    return document.getElementById(id);
+}
+
+const initializeApp = () => {
+
+    connectDOM();
+    setUpListeners();
+}
+
+const connectDOM = () => {
+    viewElements.mainContainer = getElement('mainContainer');
+    viewElements.weatherSearchView = getElement('weatherSearchView');
+    viewElements.searchInput = getElement('searchInput');
+    viewElements.searchButton = getElement('searchButton');
+
+    viewElements.weatherForecastView = getElement('weatherForecastView');
+    viewElements.returnToSearchBtn = getElement('returnToSearchBtn');
+
+    viewElements.weatherCity = getElement('weatherCity');
+    viewElements.weatherIcon = getElement('weatherIcon');
+
+    viewElements.weatherCurrentTemp = getElement('weatherCurrentTemp');
+    viewElements.weatherMaxTemp = getElement('weatherMaxTemp');
+    viewElements.weatherMinTemp = getElement('weatherMinTemp');
+    
+}
+
+const setUpListeners = () => {
+    viewElements.searchInput.addEventListener(keydown, pressEnter);
+    viewElements.searchButton.addEventListener(click, pressButton);
+}
