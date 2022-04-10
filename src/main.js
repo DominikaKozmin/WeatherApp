250e5d48117c428b80db29b1151d46a7c55b0718
Dominika Koźmin <kozmindominika@gmail.com>
2022-04-10 21:10:38 +0200

    Fixes and created fadeInOut function

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 import {getWeatherByCity} from './apiService.js';
 
-
-document.addEventListener('DOMContentLoaded', initializeApp);
-
 const viewElements = {};
-
-const getElement = id => {
-    return document.getElementById(id);
-}
 
 const initializeApp = () => {
 
     connectDOM();
     setUpListeners();
+}
+
+document.addEventListener('DOMContentLoaded', initializeApp);
+
+
+const getElement = id => {
+    return document.getElementById(id);
 }
 
 const connectDOM = () => {
@@ -34,13 +34,39 @@ const connectDOM = () => {
 }
 
 const setUpListeners = () => {
-    viewElements.searchInput.addEventListener(keydown, pressEnter);
-    viewElements.searchButton.addEventListener(click, pressButton);
+    viewElements.searchInput.addEventListener('keydown', pressEnter);
+    viewElements.searchButton.addEventListener('click', pressButton);
+    viewElements.returnToSearchBtn.addEventListener('click', switchView);
 }
 
 const pressEnter = event => {
-    let query = viewElements.searchInput.value;
     if (event.key === 'Enter') {
+        let query = viewElements.searchInput.value;
         getWeatherByCity(query);
+        switchView();
+    }
+}
+
+const pressButton = () => {
+    let query = viewElements.searchInput.value;
+    getWeatherByCity(query);
+    switchView();
+}
+
+const switchView = () => {
+    if (viewElements.weatherSearchView.style.display !== "none") {
+        viewElements.weatherSearchView.style.display = "none";
+        viewElements.weatherForecastView.style.display = "block";
+    }else {
+        viewElements.weatherForecastView.style.display = "none";
+        viewElements.weatherSearchView.style.display = "block";
+    }
+}
+
+const fadeInOut = () => {
+    if (viewElements.mainContainer.style.opacity === "1" || viewElements.mainContainer.style.opacity === " " ) {
+        viewElements.mainContainer.style.opacity = "0";
+    }else {
+        viewElements.mainContainer.style.opacity = "1";
     }
 }
